1aa1970907ff359a67790edab952cd2aa6eb56cc
jumpvin <jumpvince@gmail.com>
2019-11-23 15:23:17 +0100

    feat: add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to LuvMoney</h1>
         <p>
           Edit
           <code>src/App.js</code>
