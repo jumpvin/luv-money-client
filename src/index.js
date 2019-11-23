b18c416b3f4f00da3660c5c5991330e505201a5f
jumpvin <jumpvince@gmail.com>
2019-11-23 13:38:57 +0100

    feat: typescript config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.tsx';
 import * as serviceWorker from './serviceWorker';
 
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
