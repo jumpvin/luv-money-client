27e8c3a2591d72fc840eb7ca3597ddc7c83b2133
Anurag Dalmia <anuragdalmiya@gmail.com>
2019-11-27 09:39:15 +0100

    feat:sign-in routing and tokens

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = async (type, url, args) => {
-  const res = await axios[type](`http://localhost:3005${url}`, args);
+const api = async (type, url, args, headers) => {
+  const res = await axios[type](`http://localhost:3001${url}`, args, headers);
   return res.data;
 };
 
@@ -9,7 +9,12 @@ const getExamples = () => api('get', '/examples');
 
 const setExamples = () => api('post', '/example');
 
+const getPool = (args, headers) => api('get', '/pool', args, headers);
+
+const getUsers = (args) => api('get', '/users', args);
+
 export {
   getExamples,
   setExamples,
+  getPool,
 };
