bce29f94f2fb9e432554ec0922e1b98f055a1745
TheeBriBri <brianabarlowucla@gmail.com>
2019-11-28 10:02:48 +0100

    feat: payment started

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import StatementCard from '../../molecules/StatementCard';
+import { useState, useEffect } from 'react';
+import { getPool } from '../../services/api'
 
-const Payment = () => (
+
+
+const Payment = () => {
+  // const [pool, setPool] = useState([]);
+  // useEffect(() => {
+  //   getPool()
+  // }, []);
+
+  return (
   <div>
     Payment
+    <StatementCard
+      pool={"LuvMoneyPool"}
+      amount="Balance Connection"
+    ></StatementCard>
   </div>
-);
+  )
+};
 
 export default Payment;
