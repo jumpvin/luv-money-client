07ecb47da49796c5d17409c2de81c12f0d769194
jlaing5 <james.laing5@gmail.com>
2019-11-28 13:09:26 +0100

    merge commit

--- src/pages/PoolSettings/index.tsx
+++ src/pages/PoolSettings/index.tsx
@@ -1,64 +1,77 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../../molecules/Header';
 import Card from '../../molecules/Card';
 import './pool-settings.css'
 import AddButton from '../../atoms/AddButton';
+import moment from 'moment';
 
-const PoolSettings = () => (
+
+const PoolSettings = () => {
+  const {poolSettingsInfo, userInfo, poolRuleSettingsInfo, statementInfo} = 
+    useSelector (state => ({
+      poolSettingsInfo: state.getPool.pool.poolSettingsInfo,
+      userInfo: state.getPool.pool.userInfo,
+      poolRuleSettingsInfo: state.getPool.pool.poolRuleSettingsInfo,
+      statementInfo: state.getPool.pool.statementInfo
+    })
+  );
+  
+  return (
   <div>
     <Header>
-      LuvMoney Settings
+    {poolSettingsInfo[0].name} Settings
     </Header>
 
     <Card addClass='settingsGroup'>
-      <div className='settingsTitle'>Your Pool</div>
-      <div className='settingsItem'>
-        <div>Members</div>
-        <div>4</div>
-      </div>
-      <div className='settingsItem'>
-        <div>Notifications</div>
-        <div>On</div>
-      </div>
+      <div className='left'>
+        <div className='settingsTitle'>Your Pool</div>
+        <div className='settingsItem'>
+          <div>Members</div>
+          <div>{userInfo.length}</div>
+        </div>
       <div className='settingsItem'>
         <div>Frequency</div>
-        <div>Monthly</div>
+        <div>{poolSettingsInfo[0].frequency}</div>
       </div>
       <div className='settingsItem'>
         <div>Grace Period</div>
-        <div>5 Days</div>
+        <div>{poolSettingsInfo[0].grace_period}</div>
       </div>
+      
       <div className='settingsItem'>
         <div>Next Statement Due</div>
-        <div>December 12th, 2019</div>
+        <div>{moment(poolSettingsInfo[0].statement_date).format("MMM Do YYYY")}</div>
       </div>
+    </div>
     </Card>
     <Card addClass='settingsGroup'>
+    <div className='left'>
       <div className='settingsTitle'>
         <div>Your Expense Rules</div>
         <AddButton/>
       </div>
+      {poolRuleSettingsInfo.map((rule) => (
       <div className='settingsItem'>
-        <div>Groceries</div>
+        <div>{rule.name}</div>
         <div>Edit</div>
       </div>
-      <div className='settingsItem'>
-        <div>Entertainment</div>
-        <div>Edit</div>
-      </div>
+      ))}
+    </div>
     </Card>
     <Card addClass='settingsGroup'>
-      <div className='settingsTitle'>Previous Statements</div>
+    <div className='left'>
+      <div className='settingsTitle'>Statements</div>
+      {statementInfo.map((statement) => (
       <div className='settingsItem'>
-        <div>November 12th, 2019</div>
+        <div>{moment(statement.statement_date).format("MMM Do YYYY")}</div>
         <div>View</div>
       </div>
-      <div className='settingsItem'>
-        <div>October 12th, 2019</div>
-        <div>View</div>
-      </div>
+      ))}
+    </div>
     </Card>
   </div>
 );
+  };
 
 export default PoolSettings;
