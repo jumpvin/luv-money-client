9233a4d9a9f96ff60a396e278a77022e0b435a41
jumpvin <jumpvince@gmail.com>
2019-11-30 11:37:28 +0100

    feat: Invite Sends Email

--- src/ducks/sagas.js
+++ src/ducks/sagas.js
@@ -3,12 +3,14 @@ import watchExample from './example/exampleSagas';
 import watchGetPool from './getPool/getPoolSagas';
 import watchNewRule from './newRule/newRuleSagas';
 import watchPopUpFetch from './popUpState/popUpSagas';
+import watchInvite from './invite/inviteSagas';
 
 export default function* startForman() {
   yield all([
     watchExample(),
     watchGetPool(),
     watchNewRule(),
-    watchPopUpFetch()
+    watchPopUpFetch(),
+    watchInvite(),
   ]);
 }
