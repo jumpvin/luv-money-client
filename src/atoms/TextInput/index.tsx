f36f4ba235e8c373e1417d43be3b4c3c85cc7489
Anurag Dalmia <anuragdalmiya@gmail.com>
2019-12-04 12:10:32 +0100

    feat: add expense fixed

--- src/atoms/TextInput/index.tsx
+++ src/atoms/TextInput/index.tsx
@@ -6,14 +6,15 @@ interface TextInputParams{
   type: string;
   name: string;
   value: string;
+  onChange():string;
 }
 
-const TextInput = ({ type,name,value}: RouteComponentProps<TextInputParams>)=>{
+const TextInput = ({ type,name,value,onChange}: RouteComponentProps<TextInputParams>)=>{
   return (
     // <input type={type} className='input' name={name} value={value} required ></input>
 
     <div className="group">      
-      <input type="text" required></input>
+      <input type={type} value={value} onChange={onChange} required></input>
       <span className="highlight"></span>
       <span className="bar"></span>
       <label>{name}</label>
